fdb42ced6e632e19428040b7fdc0dacf7d02c781
Curtis J Pierce <Curtisjpierce39@gmail.com>
2024-08-21 18:38:49 -0700

    add initial description

--- The Calculator App.py
+++ The Calculator App.py
@@ -1,72 +1,4 @@
-# #Question 1 Task 1
-
-# def add(x, y):
-#     return x + y
-
-# def subtract(x, y):
-#     return x - y
-
-# def multiply(x, y):
-#     return x * y
-
-# def divide(x, y):
-#     return x / y
-
-# print("Select operation.")
-# print("1.Add")
-# print("2.Subtract")
-# print("3.Multiply")
-# print("4.Divide")
-
-# #Question 1 Task 2
-
-# def add(x, y):
-#     return x + y
-
-# def subtract(x, y):
-#     return x - y
-
-# def multiply(x, y):
-#     return x * y
-
-# def divide(x, y):
-#     return x / y
-
-
-# print("Select operation.")
-# print("1.Add")
-# print("2.Subtract")
-# print("3.Multiply")
-# print("4.Divide")
-
-# while True:
-#     choice = input("Enter choice(1/2/3/4): ")
-
-#     if choice in ('1', '2', '3', '4'):
-#             num1 = float(input("Enter first number: "))
-#             num2 = float(input("Enter second number: "))
-
-#             if choice == '1':
-#                 print(num1, "+", num2, "=", add(num1, num2))
-
-#             elif choice == '2':
-#                 print(num1, "-", num2, "=", subtract(num1, num2))
-
-#             elif choice == '3':
-#                 print(num1, "*", num2, "=", multiply(num1, num2))
-
-#             elif choice == '4':
-#                 print(num1, "/", num2, "=", divide(num1, num2))
-        
-#             next_calculation = input("Let's do next calculation? (yes/no): ")
-#             if next_calculation == "no":
-#                 break
-#             else:
-#                 print("Invalid Input")
-
-
-#Question 1 Task 3
-
+#Question 1 Task 1
 
 def add(x, y):
     return x + y
@@ -80,14 +12,13 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-while True:
+while True:#Question 1 Task 2
     choice = input("Enter choice(1/2/3/4): ")
 
     if choice in ('1', '2', '3', '4'):
@@ -110,7 +41,7 @@ while True:
         elif choice == '4':
             try:
                 print(num1, "/", num2, "=", divide(num1, num2))
-            except ZeroDivisionError:
+            except ZeroDivisionError:#Question 1 Task 3
                 print("Number cannot be divided by 0. Please try again.")
                 continue
         
